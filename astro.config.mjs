cf5d0ee6408a33cee1318d0a2a366b600bf87719
Esteban Victorio <steve@Estebans-Mac-Studio.local>
2026-03-04 15:35:23 -0800

    feat: add github action for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nutrifit-app.github.io",
+  base: import.meta.env.BASE_PATH,
   server: {
     port: 3000,
   },
-
   integrations: [icon()],
-
   vite: {
     plugins: [tailwindcss()]
   }
