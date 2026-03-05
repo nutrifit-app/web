2da37b553b953322acfbcb7eade7741e298a1ec5
Esteban Victorio <steve@Estebans-Mac-Studio.local>
2026-03-04 18:39:20 -0800

    feat: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://nutrifit-app.github.io",
-  base: import.meta.env.BASE_PATH,
   server: {
     port: 3000,
   },
